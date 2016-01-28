066a16735e2d6e2a92f47a6748c672f2ab6bfa87
Irl Nathan <irlnathan@gmail.com>
2016-01-28 01:02:33 -0600

    added another token request

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,25 +20,29 @@ $(document).ready(function(){
     // Attempt a request to /csrfToken with CORS
   $('#request-csrf-token-cors-button').click(function(){
 
-    console.log('hi')
-
-    // $.ajax({
-    //   url: 'https://corsoncsrfon.herokuapp.com/csrfToken',
-    //   type: 'GET',
-    //   success: function(result){
-    //     console.log('result: ', result);
-    //   },
-    //   error: function(xhr, status, err){
-    //     console.log(err);
-    //   }
-    // });
-
     $.ajax({
       url: 'https://corsoncsrfon.herokuapp.com/csrfToken',
       type: 'GET',
       xhrFields: {
         withCredentials: true
       },
+      success: function(result) {
+        console.log(result);
+      },
+      error: function(a,b,c) {
+
+      }
+    });
+  });
+
+  // Attempt a request to /csrfToken with CSRF origin configured
+  $('#request-csrf-token-origin-button').click(function(){
+
+    console.log('hi')
+
+    $.ajax({
+      url: 'https://csrfontokenon.herokuapp.com/csrfToken',
+      type: 'GET',
       success: function(result) {
         console.log(result);
       },
