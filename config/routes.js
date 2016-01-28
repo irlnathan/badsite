a6ea6c795a1acfa4888b73bb88347151efb29039
Irl Nathan <irlnathan@gmail.com>
2016-01-27 23:26:50 -0600

    added second winning page

--- config/routes.js
+++ config/routes.js
@@ -34,6 +34,10 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
+  },
+
+  '/csrftokenon': {
+    view: 'csrftokenon'
   }
 
   /***************************************************************************
